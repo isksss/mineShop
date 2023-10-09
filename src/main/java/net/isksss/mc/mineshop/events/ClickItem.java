52eb90be9a502a7bd890fa88c18e58ed7892724d
isksss <104404522+isksss@users.noreply.github.com>
2023-10-09 19:42:22 +0900

    test

--- src/main/java/net/isksss/mc/mineshop/events/ClickItem.java
+++ src/main/java/net/isksss/mc/mineshop/events/ClickItem.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -35,6 +36,11 @@ public class ClickItem implements Listener {
             //通常購入処理
             e.setCancelled(true);
             int slotNo = e.getSlot();
+            // hotbarを選択したときは無効にする
+            InventoryType.SlotType. slottype = e.getSlotType();
+            if(slottype == InventoryType.PLAYER){
+                return;
+            }
             if(slotNo == 0){
                 return;
             }
